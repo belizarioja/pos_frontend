432728f38d4e32989f6be8dc4ec07b66d191af35
Desarrollador 1 IR <desarrollador1@intelicorps.com>
2024-06-14 15:22:55 -0400

    Login Finalizado totalmente responsive

--- src/layouts/LoginLayout.vue
+++ src/layouts/LoginLayout.vue
@@ -1,52 +1,62 @@
 <template>
+  <q-layout view="hHh lpR fFf">
+    <q-page-container>
+      <q-page class="flex flex-center">
+        <div class="container">
 
-  <div class="full-height full-width q-pa-md p-0 no-scroll-y">
-    <div class="row q-col-gutter-md full-height">
-      <div class="left-image col-12 col-md-7 flex flex-center column">
-        <h4 class="text-left q-mb-md slogan">
-          Te ofrecemos la plataforma <span class="text-resaltado">más fácil, segura y confiable</span> del mercado.
-        </h4>
-        <img src="img/logo_smart.avif" alt="Pequeña Imagen" class="small-image q-mt-md">
-      </div>
-
-      <div class="col-12 col-md-5 bg-color-yellow flex flex-center">
-        <div class="center-form q-pa-md">
-          <h5 class="titulo">¡Bienvenido a Smart <br><b>proSales</b>!</h5>
-          <form @submit.prevent="submit" class="form-width">
-            <q-card bordered class="q-pa-lg custom-shadow custom-border-radius">
-              <h6 class="p-0"><b>Iniciar sesión</b></h6>
-              <q-card-section>
-                <q-form class="q-gutter-md">
-                  <q-input outlined clearable v-model="usuarioCentro" type="text" label="Usuario">
-                    <template v-slot:prepend>
-                      <q-icon name="account_circle" />
+          <div class="left-pane">
+            <div class="left-content">
+              <div class="left-text">
+                <h4 class="text-left q-mb-md slogan">
+                  Te ofrecemos la plataforma <span class="text-resaltado">más fácil, segura y confiable</span> del
+                  mercado.
+                </h4>
+              </div>
+              <div class="left-logo">
+                <img src="img/logo_smart.avif" alt="Logo" class="img-logo">
+              </div>
+            </div>
+          </div>
+          <div class="right-pane">
+            <div class="text-h6 text-center color-texto mb-2 mt-2">¡Bienvenido a Smart<br><b>proSales</b>! 🧾</div>
+            <!-- Contenido de la mitad derecha -->
+            <q-card bordered class="q-pa-md custom-shadow custom-border-radius card-width">
+              <div class="text-h6 text-center color-texto">Iniciar sesión</div>
+              <form @submit.prevent="submit" class="form-width-small">
+                <q-card-section>
+                  <q-form class="q-gutter-sm">
+                    <q-input outlined clearable v-model="usuarioCentro" type="text" label="Usuario">
+                      <template v-slot:prepend>
+                        <q-icon name="account_circle" />
+                      </template>
+                    </q-input>
+                    <q-input outlined clearable v-model="claveCentro" type="password" label="Clave">
+                      <template v-slot:prepend>
+                        <q-icon name="lock" />
+                      </template>
+                    </q-input>
+                  </q-form>
+                </q-card-section>
+                <q-card-actions class="q-px-sm q-mt-sm">
+                  <q-btn no-caps unelevated class="gradient-btn full-width" type="submit" :loading="loading" size="md"
+                    label="Ingresar">
+                    <template v-slot:loading>
+                      <q-spinner-facebook />
                     </template>
-                  </q-input>
-                  <q-input outlined clearable v-model="claveCentro" type="password" label="Clave">
-                    <template v-slot:prepend>
-                      <q-icon name="lock" />
-                    </template>
-                  </q-input>
-                </q-form>
-              </q-card-section>
-              <q-card-actions class="q-px-md">
-                <q-btn no-caps unelevated class="gradient-btn full-width" type="submit" :loading="loading" size="lg"
-                  label="Ingresar">
-                  <template v-slot:loading>
-                    <q-spinner-facebook />
-                  </template>
-                </q-btn>
-              </q-card-actions>
-              <hr />
-              <img src="img/logo-prosales.png" alt="Logo" class="img-logo q-mx-auto">
+                  </q-btn>
+                </q-card-actions>
+              </form>
+              <q-separator />
+              <div class="q-mt-sm text-center">
+                <img src="img/logo-prosales.png" alt="Logo" class="img-logoPro q-mx-auto">
+              </div>
             </q-card>
-          </form>
+          </div>
         </div>
-      </div>
-    </div>
-  </div>
+      </q-page>
+    </q-page-container>
+  </q-layout>
 </template>
-
 <script>
 import { Notify } from 'quasar'
 import axios from 'axios'
@@ -103,93 +113,104 @@ export default {
 </script>
 
 <style scoped>
-.full-height {
+.container {
+  display: flex;
   height: 100vh;
-}
-
-.full-width {
-  width: 100vw;
-}
-
-.no-scroll-y {
-  overflow-y: hidden;
-}
-
-.form-width {
   width: 100%;
-  max-width: 350px;
 }
 
-.left-image {
-  background: linear-gradient(360deg, rgba(204, 95, 2, 0.95) 0%, rgba(204, 95, 2, 0) 68%), url('img/log.avif');
-  background-position: center;
+.left-pane {
+  flex: 0.6;
+  background: linear-gradient(360deg, rgba(204, 95, 2, 0.95) 0%, rgba(204, 95, 2, 0) 68%), url('img/log.avif') no-repeat center center;
   background-size: cover;
-  padding: 10%;
-  margin-top: 1%;
-  border-radius: 5%;
 }
 
-.slogan {
-  margin: 0
+.left-content {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
 }
 
-.titulo {
-  margin-bottom: 5%;
-  margin-top: -10%;
-
-}
-
-.center-form {
-  display: grid;
-  justify-content: center;
+.left-text {
+  margin-top: auto;
+  margin-bottom: 5px;
   text-align: center;
+  padding-left: 20px;
 }
 
-.img-logo {
-  width: 65%;
-  padding: 4%;
-}
-
-.custom-shadow {
-  box-shadow: 0px 10px 10px rgba(139, 139, 139, 0.25);
-}
-
-.custom-border-radius {
-  border-radius: 20px;
-}
-
-.left-image h4 {
-  margin-top: 60%;
+.left-text h4 {
   font-weight: bold;
-  color: #FFFAEC;
+  color: #fffaec;
 }
 
 .text-resaltado {
-  color: #FFE6A5;
+  color: #ffe6a5;
 }
 
-.gradient-btn {
-  background: linear-gradient(90deg, #FF9A0A 39%, #FFD26D 100%);
-  color: #fff;
-  border: none;
+.left-logo {
+  text-align: center;
 }
 
-.gradient-btn:hover {
-  opacity: 0.9;
+.right-pane {
+  flex: 0.4;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
 }
 
-.gradient-btn.q-btn--unelevated {
-  box-shadow: none;
+.img-logo {
+  width: 30%;
+  padding: 4%;
 }
 
-.small-image {
-  width: 150px;
-  /* Ajusta el tamaño de la imagen según tus necesidades */
-  height: auto;
-  margin: 0;
+.img-logoPro {
+  width: 50%;
+}
 
-  filter: drop-shadow(0 0 20px rgb(255, 251, 227));
-  /* Sombra blanca */
+.form-width {
+  max-width: 350px;
+  margin: 0 auto;
+}
 
+.form-width-small {
+  max-width: 300px;
+  margin: 0 auto;
+}
+
+.card-width {
+  max-width: 350px;
+  width: 100%;
+}
+
+/* Media query para teléfonos móviles */
+@media (max-width: 600px) {
+  .container {
+    flex-direction: column-reverse;
+  }
+
+  .left-pane {
+    flex: 1;
+    order: 1;
+  }
+
+  .right-pane {
+    flex: 1;
+    order: 2;
+  }
+
+  .left-content {
+    justify-content: center;
+  }
+
+  .left-text {
+    padding-left: 0;
+  }
+
+  .img-logo {
+    width: 50%;
+  }
 }
 </style>
